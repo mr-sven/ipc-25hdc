a1b3644955559ccfdf8ed2e6b5768a5dda0153e6
Sven <sven.fabricius@livediesel.de>
2017-02-28 22:04:18 +0100

    wrong filename

--- source/vendors/Ralink/RT5350/init/02_etc.sh
+++ source/vendors/Ralink/RT5350/init/02_etc.sh
@@ -8,8 +8,8 @@ ln -s /etc_ro/profile /etc/profile
 
 # generate default config for factory reset
 mkdir -p /etc/default
-cp /etc_ro/default/FACTORY.dat /etc/FACTORY.dat
-cat etc_ro/default/RT2860.dat >> /etc/FACTORY.dat
-echo factory_mac=`nvram_get factory_mac` >> /etc/FACTORY.dat
-echo factory_wifimac=`nvram_get factory_wifimac` >> /etc/FACTORY.dat
-echo WAN_MAC_ADDR=`nvram_get WAN_MAC_ADDR` >> /etc/FACTORY.dat
+cp /etc_ro/default/FACTORY.dat /etc/default/FACTORY.dat
+cat etc_ro/default/RT2860.dat >> /etc/default/FACTORY.dat
+echo factory_mac=`nvram_get factory_mac` >> /etc/default/FACTORY.dat
+echo factory_wifimac=`nvram_get factory_wifimac` >> /etc/default/FACTORY.dat
+echo WAN_MAC_ADDR=`nvram_get WAN_MAC_ADDR` >> /etc/default/FACTORY.dat
