f9911a47ccc908da8cf767b2498295b0e108e56d
Sven <sven.fabricius@livediesel.de>
2017-03-01 14:25:23 +0100

    updated factory setting

--- source/vendors/Ralink/RT5350/init/02_etc.sh
+++ source/vendors/Ralink/RT5350/init/02_etc.sh
@@ -9,7 +9,8 @@ ln -s /etc_ro/profile /etc/profile
 # generate default config for factory reset
 mkdir -p /etc/default
 cp /etc_ro/default/FACTORY.dat /etc/default/FACTORY.dat
-cat etc_ro/default/RT2860.dat >> /etc/default/FACTORY.dat
+
+# preserve factory mac
 echo factory_mac=`nvram_get factory_mac` >> /etc/default/FACTORY.dat
 echo factory_wifimac=`nvram_get factory_wifimac` >> /etc/default/FACTORY.dat
 echo WAN_MAC_ADDR=`nvram_get WAN_MAC_ADDR` >> /etc/default/FACTORY.dat
