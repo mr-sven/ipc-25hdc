0a729f07018d7caa4dfd35f8a28a0ef3ce9b87d2
Sven <sven.fabricius@livediesel.de>
2019-01-28 12:54:42 +0100

    adding commandline parser

--- source/user/v4l2aitcontrol/v4l2aitcontrol.c
+++ source/user/v4l2aitcontrol/v4l2aitcontrol.c
@@ -9,46 +9,80 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <getopt.h>
+#include <byteswap.h>
 
 #include "AitXU.h"
 
+void print_usage(char *progname)
+{
+	fprintf(stderr, "------------------------------------------------------------------\n");
+	fprintf(stderr, "Usage: %s\n" \
+					" [-h | --help ]........: display this help\n" \
+					" [-v | --version ].....: display version information\n" \
+					" [-i | --init].........: init Ait Device\n", progname);
+	fprintf(stderr, "------------------------------------------------------------------\n");
+}
+
+void init_ait(int fd)
+{
+	__u8 data[8];
+
+	AitXU_Init(fd);
+
+	memset(data, 0, 8);
+
+	AitXU_GetFWBuildDate(fd, data, 8);
+	printf("AIT Firmware Build Date: 20%.2s.%.3s.%.2s\n", &data[1], &data[3], &data[6]);
+
+    struct versionDev {
+		__u16 reseverd;
+		__u16 major;
+		__u16 minor;
+		__u16 patch;
+    } version;
+
+	AitXU_GetFWVersion(fd, (__u8 *)&version, sizeof(version));
+	printf("AIT Firmware Build Date: %d.%d.%d\n", __bswap_16(version.major), __bswap_16(version.minor), __bswap_16(version.patch));
+}
+
 int main(int argc, char *argv[])
 {
-    char *dev = "/dev/video0";
-    int fd = 0;
+	char *dev = "/dev/video0";
+	int fd = 0;
+	int opt = 0;
 
-    __u8 data[8];
+	if ((fd = open(dev, O_RDWR)) == -1)
+	{
+		fprintf(stderr, "ERROR opening V4L interface \n");
+		exit(1);
+	}
 
-    if ((fd = open(dev, O_RDWR)) == -1) 
-    {
-        perror("ERROR opening V4L interface \n");
-        exit(1);
-    }
+	static struct option long_options[] = {
+		{"help",		no_argument,		0, 'h'},
+		{"init",		no_argument,		0, 'i'},
+		{0,				0,					0, 0}
+	};
 
-    AitXU_Init(fd);
-
-    memset(data, 0, 8);
-    AitXU_GetFWBuildDate(fd, data, 8);
-
-    int cnt;
-    for (cnt = 0; cnt < 8; cnt++) {
-        printf(" 0x%02x", data[cnt]);
-    }
-
-    printf("\n");
-
-    memset(data, 0, 8);
-    AitXU_GetFWVersion(fd, data, 8);
-
-    for (cnt = 0; cnt < 8; cnt++) {
-        printf(" 0x%02x", data[cnt]);
-    }
-    printf("\n");
-
-    H264SetMirr(fd, AIT_FM_NORMAL);
-    H264SetIRCutMode(fd, AIT_IR_NIGHT);
-
-    AitXU_SetFrameRate(fd, 5);
-
-    close(fd);
+	int long_index =0;
+	while ((opt = getopt_long_only(argc, argv,"", long_options, &long_index )) != -1)
+	{
+		switch (opt)
+		{
+			case 'i' :
+				init_ait(fd);
+				break;
+			default:
+				print_usage(argv[0]);
+				close(fd);
+				exit(EXIT_FAILURE);
+		}
+	}
+	close(fd);
+//
+//	H264SetMirr(fd, AIT_FM_NORMAL);
+//	H264SetIRCutMode(fd, AIT_IR_NIGHT);
+//
+//	AitXU_SetFrameRate(fd, 5);
+//
+//	close(fd);
 }
