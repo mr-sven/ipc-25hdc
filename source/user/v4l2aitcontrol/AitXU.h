ba6bea959add638986e0cbdf6490d27f83df3096
Sven <sven.fabricius@livediesel.de>
2019-01-28 10:25:02 +0100

    code cleanup

--- source/user/v4l2aitcontrol/AitXU.h
+++ source/user/v4l2aitcontrol/AitXU.h
@@ -2,62 +2,62 @@
 #ifndef _AITXU_H_
 #define _AITXU_H_
 
-#define UVC_SET_CUR					0x01
-#define UVC_GET_CUR					0x81
+#define UVC_SET_CUR							0x01
+#define UVC_GET_CUR							0x81
 
 #define UVC_VC_EXTENSION_UNIT				0x06
 
-#define UVC_GUID_AIT_EXTENSION      {0xd0, 0x9e, 0xe4, 0x23, 0x78, 0x11, 0x31, 0x4f, 0xae, 0x52, 0xd2, 0xfb, 0x8a, 0x8d, 0x3b, 0x48}
-#define V4L2_AIT_XU_ID_BASE         0x08000000
+#define UVC_GUID_AIT_EXTENSION				{0xd0, 0x9e, 0xe4, 0x23, 0x78, 0x11, 0x31, 0x4f, 0xae, 0x52, 0xd2, 0xfb, 0x8a, 0x8d, 0x3b, 0x48}
+#define V4L2_AIT_XU_ID_BASE					0x08000000
 
-#define AIT_XU_GET                  (UVC_CONTROL_GET_CUR | UVC_CONTROL_GET_DEF | UVC_CONTROL_GET_MIN | UVC_CONTROL_GET_MAX)
-#define AIT_XU_SET                  (UVC_CONTROL_SET_CUR | UVC_CONTROL_GET_DEF | UVC_CONTROL_GET_MIN | UVC_CONTROL_GET_MAX)
+#define AIT_XU_GET							(UVC_CONTROL_GET_CUR | UVC_CONTROL_GET_DEF | UVC_CONTROL_GET_MIN | UVC_CONTROL_GET_MAX)
+#define AIT_XU_SET							(UVC_CONTROL_SET_CUR | UVC_CONTROL_GET_DEF | UVC_CONTROL_GET_MIN | UVC_CONTROL_GET_MAX)
 
-#define AIT_SET_ISP_CONTROL         1
-#define AIT_GET_ISP_CONTROL         2
-#define AIT_SET_MMP_CONTROL         4
-#define AIT_GET_MMP_CONTROL         5
-#define AIT_SET_MMP16_CONTROL       14
-#define AIT_GET_MMP16_CONTROL       15
-#define AIT_SET_ISPEX_CONTROL       6
-#define AIT_GET_ISPEX_CONTROL       7
-#define AIT_SET_MMPMEM_CONTROL      9
-#define AIT_GET_MMPMEM_CONTROL      8
+#define AIT_SET_ISP_CONTROL					1
+#define AIT_GET_ISP_CONTROL					2
+#define AIT_SET_MMP_CONTROL					4
+#define AIT_GET_MMP_CONTROL					5
+#define AIT_SET_ISPEX_CONTROL				6
+#define AIT_GET_ISPEX_CONTROL				7
+#define AIT_GET_MMPMEM_CONTROL				8
+#define AIT_SET_MMPMEM_CONTROL				9
+#define AIT_SET_MMP16_CONTROL				14
+#define AIT_GET_MMP16_CONTROL				15
 
 #define UVC_XUCMD_SETFRAMERATE				0x02
-#define UVC_XUCMD_SETIFRAME				0x04
+#define UVC_XUCMD_SETIFRAME					0x04
 #define UVC_XUCMD_SETBITRATE				0x08
 #define UVC_XUCMD_GETFWVERSION				0x0B
 #define UVC_XUCMD_GETFWBUILDDATE			0x0C
-#define UVC_XUCMD_EXT_CMD				0xFF
+#define UVC_XUCMD_EXT_CMD					0xFF
 
 #define AIT_XUCMD_EXT_H264SETMIRROR			0x11
-#define AIT_XUCMD_EXT_MJPGSETQUALITY			0x13
-#define AIT_XUCMD_EXT_H264SETIRCUTMODE			0x27
+#define AIT_XUCMD_EXT_MJPGSETQUALITY		0x13
+#define AIT_XUCMD_EXT_H264SETIRCUTMODE		0x27
 
-#define AIT_FM_NORMAL					0
-#define AIT_FM_FLIP					1
-#define AIT_FM_MIRROR					2
+#define AIT_FM_NORMAL						0
+#define AIT_FM_FLIP							1
+#define AIT_FM_MIRROR						2
 
-#define AIT_IR_DEFAULT					0
-#define AIT_IR_DAY					1
-#define AIT_IR_NIGHT					2
+#define AIT_IR_DEFAULT						0
+#define AIT_IR_DAY							1
+#define AIT_IR_NIGHT						2
 
 typedef struct uvc_xu_tbl_info {
-    __u8 name[32];
-    __u8 selector;
-    __u16 size;
-    __u8 offset;
-    __u32 flag;
-    enum v4l2_ctrl_type v4l2_type;
-    __u32 data_type;
+	__u8 name[32];
+	__u8 selector;
+	__u16 size;
+	__u8 offset;
+	__u32 flag;
+	enum v4l2_ctrl_type v4l2_type;
+	__u32 data_type;
 } uvc_xu_tbl_info;
 
 struct AitIspCmd {
-    __u8 cmd0;
-    __u8 cmd1;
-    __u16 data0;
-    __u32 data1;
+	__u8 cmd0;
+	__u8 cmd1;
+	__u16 data0;
+	__u32 data1;
 };
 
 int AitXU_Init(int fd);
