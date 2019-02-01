864d5d6d5b5aba6863e43e25e96fba2ec97ab066
Sven <sven.fabricius@livediesel.de>
2019-02-01 15:28:07 +0100

    migrate to Watchdog 5.15-2

--- source/user/watchdog-5.6/include/lomount.h
+++
@@ -1,4 +0,0 @@
-extern int mount_verbose;
-extern int set_loop (const char *, const char *, int, const char *, int *);
-extern int del_loop (const char *);
-extern char * find_unused_loop_device (void);
